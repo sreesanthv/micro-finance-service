dea93b5bf78d1b2e338509c91d30667628d3368b
Sreesanth Vazhayil <sreesanthv@gmail.com>
2022-06-13 17:08:03 +0530

    Added login testcase

--- internal/controller/account_controller_test.go
+++ internal/controller/account_controller_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/go-chi/chi"
+	"github.com/go-redis/redis"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/sreesanthv/micro-finance-service/internal/domain"
 	"github.com/sreesanthv/micro-finance-service/internal/domain/mocks"
@@ -65,4 +66,51 @@ func TestCreateAccount(t *testing.T) {
 		accountController.Delete(w, req)
 		So(w.Result().StatusCode, ShouldEqual, http.StatusCreated)
 	})
+}
+
+func TestLogin(t *testing.T) {
+	input := domain.Account{
+		ID:       1,
+		Email:    "srv@test.local",
+		Password: "$2a$10$A1Eqj39/AhwL63f8G8pTke6Y3eVj1OdgxFutN9cQBcBWMhpDOagC2",
+		Name:     "Sreesanth",
+	}
+
+	accountService := &mocks.AccountService{}
+	accountService.On("GetByEmail", mock.Anything, "srv@test.local").Return(&input, nil)
+	rd := &mocks.Redis{}
+	rd.On("Set", mock.AnythingOfType("string"), 1, mock.Anything).Return(&redis.StatusCmd{})
+	rd.On("Get", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(&redis.StatusCmd{})
+	logger := &mocks.Logger{}
+	logger.On("Errorf", mock.Anything, mock.Anything)
+
+	accountController := NewAccontController(accountService, logger, rd)
+
+	Convey("Should delete account", t, func() {
+		var buf bytes.Buffer
+		bd := domain.Account{
+			Email:    "srv@test.local",
+			Password: "password",
+		}
+		err := json.NewEncoder(&buf).Encode(bd)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rctx := chi.NewRouteContext()
+		rctx.URLParams.Add("id", "1")
+
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/account/login", &buf)
+		req = req.WithContext(context.WithValue(req.Context(), "accountID", 1))
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+		accountController.Login(w, req)
+		So(w.Result().StatusCode, ShouldEqual, http.StatusOK)
+		decoder := json.NewDecoder(w.Body)
+		out := map[string]string{}
+		decoder.Decode(&out)
+		So(out["token"], ShouldNotBeEmpty)
+	})
+
 }
